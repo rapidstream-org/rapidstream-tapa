bc70c2dd46e932d89b07866bca0807f6aced3c2a
Blaok <i@blaok.me>
2019-12-09 14:16:59 -0800

    test(page-rank): report kernel runtime

--- apps/page-rank/page-rank-frt.cpp
+++ apps/page-rank/page-rank-frt.cpp
@@ -33,10 +33,12 @@ struct Update {
 void PageRank(Pid num_partitions, tlp::mmap<const Vid> num_vertices,
               tlp::mmap<const Eid> num_edges, tlp::mmap<VertexAttr> vertices,
               tlp::mmap<const Edge> edges, tlp::mmap<Update> updates) {
-  fpga::Invoke(getenv("BITSTREAM"), num_partitions,
-               fpga::WriteOnly(num_vertices.get(), num_vertices.size()),
-               fpga::WriteOnly(num_edges.get(), num_edges.size()),
-               fpga::ReadWrite(vertices.get(), vertices.size()),
-               fpga::WriteOnly(edges.get(), edges.size()),
-               fpga::ReadWrite(updates.get(), updates.size()));
+  auto instance =
+      fpga::Invoke(getenv("BITSTREAM"), num_partitions,
+                   fpga::WriteOnly(num_vertices.get(), num_vertices.size()),
+                   fpga::WriteOnly(num_edges.get(), num_edges.size()),
+                   fpga::ReadWrite(vertices.get(), vertices.size()),
+                   fpga::WriteOnly(edges.get(), edges.size()),
+                   fpga::ReadWrite(updates.get(), updates.size()));
+  LOG(INFO) << "kernel time: " << instance.ComputeTimeSeconds() << "s";
 }
