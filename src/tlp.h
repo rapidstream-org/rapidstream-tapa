652e6e311c30a8d11755e12432e023c58bcb5288
Blaok <i@blaok.me>
2020-01-03 16:10:58 -0800

    feat(libtlp): warn instead err on peeking eos

--- src/tlp.h
+++ src/tlp.h
@@ -173,7 +173,7 @@ class stream : public istream<T>, public ostream<T> {
     if (!empty()) {
       auto& elem = access(tail);
       if (elem.eos) {
-        throw std::runtime_error("stream '" + name + "' peeked when closed");
+        LOG(WARNING) << "stream '" << name << "' peeded when closed";
       }
       val = elem.val;
       return true;
