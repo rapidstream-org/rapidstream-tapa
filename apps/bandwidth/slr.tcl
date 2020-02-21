cc8daf82196bad7c9af6425bf0d7d4da9d92be32
Blaok <i@blaok.me>
2020-02-21 12:20:19 -0800

    test(bandwidth): constraint control_s_axi to SLR0

--- apps/bandwidth/slr.tcl
+++ apps/bandwidth/slr.tcl
@@ -1,5 +1,5 @@
 puts "applying slr.tcl"
-add_cells_to_pblock [get_pblocks pblock_dynamic_SLR0] [get_cells -hierarchical { Copy_0 chan0_m_axi }]
+add_cells_to_pblock [get_pblocks pblock_dynamic_SLR0] [get_cells -hierarchical { Copy_0 chan0_m_axi Bandwidth_control_s_axi_U }]
 add_cells_to_pblock [get_pblocks pblock_dynamic_SLR1] [get_cells -hierarchical { Copy_1 chan1_m_axi }]
 add_cells_to_pblock [get_pblocks pblock_dynamic_SLR2] [get_cells -hierarchical { Copy_2 chan2_m_axi }]
 add_cells_to_pblock [get_pblocks pblock_dynamic_SLR3] [get_cells -hierarchical { Copy_3 chan3_m_axi }]
