4d95efcc0e7ce77d872ae0020227671c134cf606
Blaok <i@blaok.me>
2020-03-10 14:01:52 -0700

    fix(tlpc): relay station swallows elems when full

--- backend/python/tlp/assets/verilog/relay_station.v
+++ backend/python/tlp/assets/verilog/relay_station.v
@@ -11,7 +11,7 @@ module relay_station #(
   input wire reset,
 
   // write
-  output reg                   if_full_n,
+  output wire                  if_full_n,
   input  wire                  if_write_ce,
   input  wire                  if_write,
   input  wire [DATA_WIDTH-1:0] if_din,
@@ -23,49 +23,45 @@ module relay_station #(
   output wire [DATA_WIDTH-1:0] if_dout
 );
 
-  reg if_write_q;
-  reg [DATA_WIDTH-1:0] if_din_q;
+  (* dont_touch = "yes" *) wire                  full_n  [LEVEL:0];
+  (* dont_touch = "yes" *) wire                  empty_n [LEVEL:0];
+  (* dont_touch = "yes" *) wire [DATA_WIDTH-1:0] data    [LEVEL:0];
 
-  (* dont_touch = "yes" *) wire                  full_n_all [LEVEL:0];
-  (* dont_touch = "yes" *) wire                  write_all  [LEVEL:0];
-  (* dont_touch = "yes" *) wire [DATA_WIDTH-1:0] din_all    [LEVEL:0];
+  localparam kDepthPerUnit = (DEPTH - 1) / LEVEL + 1;
 
   genvar i;
   for (i = 0; i < LEVEL; i = i + 1) begin : inst
     fifo #(
       .DATA_WIDTH(DATA_WIDTH),
       .ADDR_WIDTH(ADDR_WIDTH),
-      .DEPTH((DEPTH - 1) / LEVEL < 2 ? 2 : (DEPTH - 1) / LEVEL)
+      .DEPTH(kDepthPerUnit < 2 ? 2 : kDepthPerUnit)
     ) unit (
       .clk(clk),
       .reset(reset),
 
-      // connect to fifo i+1
-      .if_empty_n(write_all[i+1]),
+      // connect to fifo[i+1]
+      .if_empty_n(empty_n[i+1]),
       .if_read_ce(if_read_ce),
-      .if_read   (full_n_all[i+1]),
-      .if_dout   (din_all[i+1]),
+      .if_read   (full_n[i+1]),
+      .if_dout   (data[i+1]),
 
-      // connect to fifo i-1
-      .if_full_n  (full_n_all[i]),  // almost full, grace period = 2
+      // connect to fifo[i-1]
+      .if_full_n  (full_n[i]),
       .if_write_ce(if_write_ce),
-      .if_write   (write_all[i]),   // delayed input
-      .if_din     (din_all[i])      // delayed input
+      .if_write   (empty_n[i]),
+      .if_din     (data[i])
     );
   end
 
-  // connect to interface
-  always @ (posedge clk) begin
-    if_full_n  <= full_n_all[0];
-    if_write_q <= if_write;
-    if_din_q   <= if_din;
-  end
+  // write
+  assign if_full_n  = full_n[0];  // output
+  assign empty_n[0] = if_write;   // input
+  assign data[0]    = if_din;     // input
 
-  assign din_all[0]        = if_din_q;          // input
-  assign write_all[0]      = if_write_q;        // input
-  assign full_n_all[LEVEL] = if_read;           // input
-  assign if_empty_n        = write_all[LEVEL];  // output
-  assign if_dout           = din_all[LEVEL];    // output
+  // read
+  assign if_empty_n    = empty_n[LEVEL];  // output
+  assign full_n[LEVEL] = if_read;         // input
+  assign if_dout       = data[LEVEL];     // output
 
 endmodule   // relay_station
 
