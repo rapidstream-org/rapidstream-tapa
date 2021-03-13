c2c033707e7107136ed5f66290e8d761c49ed12f
Blaok <i@blaok.me>
2021-03-12 22:11:25 -0800

    test(bandwidth): leverage vectorized API

--- apps/bandwidth/bandwidth-host.cpp
+++ apps/bandwidth/bandwidth-host.cpp
@@ -15,35 +15,32 @@ int main(int argc, char* argv[]) {
   const uint64_t n = argc > 1 ? atoll(argv[1]) : 1024 * 1024;
   const uint64_t flags = argc > 2 ? atoll(argv[2]) : 6LL;
 
-  vector<Elem> chan[kBankCount];
+  vector<float> chan[kBankCount];
   for (int64_t i = 0; i < kBankCount; ++i) {
-    chan[i].resize(n);
-    for (int64_t j = 0; j < n; ++j) {
-      for (int64_t k = 0; k < Elem::length; ++k) {
-        chan[i][j].set(k, i ^ j ^ k);
-      }
+    chan[i].resize(n * Elem::length);
+    for (int64_t j = 0; j < n * Elem::length; ++j) {
+      chan[i][j] = i ^ j;
     }
   }
 
-  Bandwidth(chan, n, flags);
+  Bandwidth(tapa::mmaps<float, kBankCount>(chan).vectorized<Elem::length>(), n,
+            flags);
 
   if (!((flags & kRead) && (flags & kWrite))) return 0;
 
   int64_t num_errors = 0;
   const int64_t threshold = 10;  // only report up to these errors
   for (int64_t i = 0; i < kBankCount; ++i) {
-    for (int64_t j = 0; j < n; ++j) {
-      for (int64_t k = 0; k < Elem::length; ++k) {
-        int64_t expected = i ^ j ^ k;
-        int64_t actual = chan[i][j][k];
-        if (actual != expected) {
-          if (num_errors < threshold) {
-            LOG(ERROR) << "expected: " << expected << ", actual: " << actual;
-          } else if (num_errors == threshold) {
-            LOG(ERROR) << "...";
-          }
-          ++num_errors;
+    for (int64_t j = 0; j < n * Elem::length; ++j) {
+      int64_t expected = i ^ j;
+      int64_t actual = chan[i][j];
+      if (actual != expected) {
+        if (num_errors < threshold) {
+          LOG(ERROR) << "expected: " << expected << ", actual: " << actual;
+        } else if (num_errors == threshold) {
+          LOG(ERROR) << "...";
         }
+        ++num_errors;
       }
     }
   }
