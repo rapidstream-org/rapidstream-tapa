63149d5bfcd2c61ec314ef74fee5c5bf0ebffaab
Blaok <i@blaok.me>
2021-03-13 21:01:54 -0800

    chore: frt is now required

--- cmake/TAPAConfig.cmake
+++ cmake/TAPAConfig.cmake
@@ -1,4 +1,5 @@
 find_package(Boost 1.59 COMPONENTS coroutine stacktrace_backtrace)
+find_package(FRT REQUIRED)
 
 include("${CMAKE_CURRENT_LIST_DIR}/TAPATargets.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/TAPACCConfig.cmake")
