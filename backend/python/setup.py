f676184a42da1d272c37e4e32e46959cf2315b53
Blaok <i@blaok.me>
2021-08-01 13:50:18 -0700

    chore: bump version

--- backend/python/setup.py
+++ backend/python/setup.py
@@ -43,7 +43,7 @@ setup(
     packages=find_packages(),
     python_requires='>=3.6',
     install_requires=[
-        'haoda>=0.0.20200507.dev1',
+        'haoda>=0.0.20210801.dev1',
         'pyverilog>=1.2.0',
         'toposort',
     ],
