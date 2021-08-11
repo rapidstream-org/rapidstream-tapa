674a99fdff5323a834ca938482e725f0668e6357
Blaok <i@blaok.me>
2021-08-11 15:12:58 -0700

    feat(libtapa): support reading T::width

--- src/tapa/synthesizable/util.h
+++ src/tapa/synthesizable/util.h
@@ -1,6 +1,8 @@
 #ifndef TAPA_UTIL_H_
 #define TAPA_UTIL_H_
 
+#include <climits>
+
 #include <iostream>
 
 #include "../stream.h"
@@ -10,9 +12,39 @@ namespace tapa {
 constexpr int join = 0;
 constexpr int detach = -1;
 
+namespace internal {
+
+template <typename T, int width = T::width>
+inline constexpr int widthof(int) {
+  return T::width;
+}
+
 template <typename T>
-inline constexpr uint64_t widthof() {
+inline constexpr int widthof(short) {
   return sizeof(T) * CHAR_BIT;
+}
+
+}  // namespace internal
+
+/// Queries width (in bits) of the type.
+///
+/// @tparam T Type to be queried.
+/// @return   @c T::width if it exists, `sizeof(T) * CHAR_BIT` otherwise.
+template <typename T>
+inline constexpr int widthof() {
+  return internal::widthof<T>(0);
+}
+
+/// Queries width (in bits) of the object.
+///
+/// @note         Unlike @c sizeof, the argument expression is evaluated (though
+///               unused).
+/// @tparam T     Type of @c object.
+/// @param object Object to be queried.
+/// @return       @c T::width if it exists, `sizeof(T) * CHAR_BIT` otherwise.
+template <typename T>
+inline constexpr int widthof(T object) {
+  return internal::widthof<T>(0);
 }
 
 template <uint64_t N>
@@ -49,22 +81,22 @@ inline std::ostream& operator<<(std::ostream& os,
 #define TAPA_WHILE_NOT_EOS(fifo)                                \
   for (bool tapa_##fifo##_valid;                                \
        !fifo.eos(tapa_##fifo##_valid) || !tapa_##fifo##_valid;) \
-   _Pragma("HLS pipeline II = 1")                               \
-    if (tapa_##fifo##_valid)
+  _Pragma("HLS pipeline II = 1") if (tapa_##fifo##_valid)
 
 #define TAPA_WHILE_NEITHER_EOS(fifo1, fifo2)                          \
   for (bool tapa_##fifo1##_valid, tapa_##fifo2##_valid;               \
        (!fifo1.eos(tapa_##fifo1##_valid) || !tapa_##fifo1##_valid) && \
        (!fifo2.eos(tapa_##fifo2##_valid) || !tapa_##fifo2##_valid);)  \
-  _Pragma("HLS pipeline II = 1")                                      \
-    if (tapa_##fifo1##_valid && tapa_##fifo2##_valid)
+  _Pragma("HLS pipeline II = 1") if (tapa_##fifo1##_valid &&          \
+                                     tapa_##fifo2##_valid)
 
 #define TAPA_WHILE_NONE_EOS(fifo1, fifo2, fifo3)                              \
   for (bool tapa_##fifo1##_valid, tapa_##fifo2##_valid, tapa_##fifo3##_valid; \
        (!fifo1.eos(tapa_##fifo1##_valid) || !tapa_##fifo1##_valid) &&         \
        (!fifo2.eos(tapa_##fifo2##_valid) || !tapa_##fifo2##_valid) &&         \
        (!fifo3.eos(tapa_##fifo3##_valid) || !tapa_##fifo3##_valid);)          \
-  _Pragma("HLS pipeline II = 1")                                              \
-    if (tapa_##fifo1##_valid && tapa_##fifo2##_valid && tapa_##fifo3##_valid)
+  _Pragma("HLS pipeline II = 1") if (tapa_##fifo1##_valid &&                  \
+                                     tapa_##fifo2##_valid &&                  \
+                                     tapa_##fifo3##_valid)
 
 #endif  // TAPA_UTIL_H_
