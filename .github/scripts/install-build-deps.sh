359c4f1b4efe574dd4fcb07e8443a44615f51b5d
Blaok <i@blaok.me>
2021-08-17 12:05:24 -0700

    chore: use same script for both CI and package

---
+++ .github/scripts/install-build-deps.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+sudo apt-get purge -y libgcc-*-dev || true
+sudo apt-get install -y \
+  build-essential \
+  libboost-coroutine-dev \
+  libboost-stacktrace-dev \
+  libgflags-dev \
+  libgoogle-glog-dev \
+  python3-pip
+
+sudo apt-get autoremove -y
+sudo -H python3 -m pip install --upgrade pip==20.3.4
+sudo -H python3 -m pip install cmake
+wget -O - https://raw.githubusercontent.com/Blaok/fpga-runtime/master/install.sh | bash
