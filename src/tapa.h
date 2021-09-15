a7474cab1f24105c0fd4f600037107af345b2ad9
Blaok <i@blaok.me>
2021-09-15 16:55:16 -0700

    docs(libtapa): doxygen for tapa::task

--- src/tapa.h
+++ src/tapa.h
@@ -155,9 +155,27 @@ struct invoker<void (&)(Params...)> {
 
 #endif  // __SYNTHESIS__
 
+/// Defines a parent task instantiating children task instances.
+///
+/// Canonical usage:
+/// @code{.cpp}
+///  tapa::task()
+///    .invoke(...)
+///    .invoke(...)
+///    ...
+///    ;
+/// @endcode
+///
+/// A parent task itself does not do any computation. By default, a parent task
+/// will not finish until all its children task instances finish. Such children
+/// task instances are @a joined to their parent. The alternative is to @a
+/// detach a child from the parent. If a child task instance is instantiated and
+/// detached, the parent will no longer wait for the child task to finish.
+/// Detached tasks are very useful when infinite loops can be used.
 struct task {
 #ifndef __SYNTHESIS__
 
+  /// Constructs a @c tapa::task.
   task();
   task(task&&) = delete;
   task(const task&) = delete;
@@ -168,47 +186,66 @@ struct task {
 
 #endif  // __SYNTHESIS__
 
+  /// Invokes a task and instantiates a child task instance.
+  ///
+  /// @param func Task function definition of the instantiated child.
+  /// @param args Arguments passed to @c func.
+  /// @return     Reference to the caller @c tapa::task.
   template <typename Func, typename... Args>
-  task& invoke(Func&& f, Args&&... args) {
-    return invoke<join>(std::forward<Func>(f), "", std::forward<Args>(args)...);
-  }
-
-  template <int step, typename Func, typename... Args>
-  task& invoke(Func&& f, Args&&... args) {
-    return invoke<step>(std::forward<Func>(f), "", std::forward<Args>(args)...);
-  }
-
-  template <typename Func, typename... Args, size_t name_size>
-  task& invoke(Func&& f, const char (&name)[name_size], Args&&... args) {
-    return invoke<join>(std::forward<Func>(f), name,
+  task& invoke(Func&& func, Args&&... args) {
+    return invoke<join>(std::forward<Func>(func), "",
                         std::forward<Args>(args)...);
   }
 
-  template <int step, typename Func, typename... Args, size_t name_size>
-  task& invoke(Func&& f, const char (&name)[name_size], Args&&... args) {
+  /// Invokes a task and instantiates a child task instance with the given
+  /// instatiation mode.
+  ///
+  /// @tparam mode Instatiation mode (@c join or @c detach).
+  /// @param func  Task function definition of the instantiated child.
+  /// @param args  Arguments passed to @c func.
+  /// @return      Reference to the caller @c tapa::task.
+  template <int mode, typename Func, typename... Args>
+  task& invoke(Func&& func, Args&&... args) {
+    return invoke<mode>(std::forward<Func>(func), "",
+                        std::forward<Args>(args)...);
+  }
+
+  template <typename Func, typename... Args, size_t name_size>
+  task& invoke(Func&& func, const char (&name)[name_size], Args&&... args) {
+    return invoke<join>(std::forward<Func>(func), name,
+                        std::forward<Args>(args)...);
+  }
+
+  template <int mode, typename Func, typename... Args, size_t name_size>
+  task& invoke(Func&& func, const char (&name)[name_size], Args&&... args) {
 #ifdef __SYNTHESIS__
     f(std::forward<Args>(args)...);
 #else   // __SYNTHESIS__
     internal::invoker<Func>::template invoke<Args...>(
-        /* detach= */ step < 0, std::forward<Func>(f),
+        /* detach= */ mode < 0, std::forward<Func>(func),
         std::forward<Args>(args)...);
 #endif  // __SYNTHESIS__
     return *this;
   }
 
-  // invoke task vector without a name
-  template <int step, uint64_t length, typename Func, typename... Args>
-  task& invoke(Func&& f, Args&&... args) {
-    return invoke<step, length>(std::forward<Func>(f), "",
-                                std::forward<Args>(args)...);
+  /// Invokes a task @c n times and instantiates @c n child task
+  /// instances with the given instatiation mode.
+  ///
+  /// @tparam mode Instatiation mode (@c join or @c detach).
+  /// @tparam n    Instatiation count.
+  /// @param func  Task function definition of the instantiated child.
+  /// @param args  Arguments passed to @c func.
+  /// @return      Reference to the caller @c tapa::task.
+  template <int mode, int n, typename Func, typename... Args>
+  task& invoke(Func&& func, Args&&... args) {
+    return invoke<mode, n>(std::forward<Func>(func), "",
+                           std::forward<Args>(args)...);
   }
 
-  // invoke task vector with a name
-  template <int step, uint64_t length, typename Func, typename... Args,
-            size_t name_size>
-  task& invoke(Func&& f, const char (&name)[name_size], Args&&... args) {
-    for (int i = 0; i < length; ++i) {
-      invoke<step>(std::forward<Func>(f), std::forward<Args>(args)...);
+  template <int mode, int n, typename Func, typename... Args, size_t name_size>
+  task& invoke(Func&& func, const char (&name)[name_size], Args&&... args) {
+    for (int i = 0; i < n; ++i) {
+      invoke<mode>(std::forward<Func>(func), std::forward<Args>(args)...);
     }
     return *this;
   }
