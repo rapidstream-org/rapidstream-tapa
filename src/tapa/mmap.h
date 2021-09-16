b07e238f68f89c0312277007c39bc6e71d90d20f
Blaok <i@blaok.me>
2021-09-15 22:51:42 -0700

    docs(libtapa): doxygen for tapa::mmap

--- src/tapa/mmap.h
+++ src/tapa/mmap.h
@@ -51,32 +51,87 @@ template <typename T>
 class async_mmap;
 #endif  // __SYNTHESIS__
 
+/// Defines a view of a piece of consecutive memory with synchronous random
+/// accesses.
 template <typename T>
 #ifdef __SYNTHESIS__
 using mmap = T*;
 #else   // __SYNTHESIS__
 class mmap {
  public:
+  /// Constructs a @c tapa::mmap with unknown size.
+  ///
+  /// @param ptr Pointer to the start of the mapped memory.
   mmap(T* ptr) : ptr_{ptr}, size_{0} {}
+
+  /// Constructs a @c tapa::mmap with the given @c size.
+  ///
+  /// @param ptr  Pointer to the start of the mapped memory.
+  /// @param size Size of the mapped memory (in unit of element count).
   mmap(T* ptr, uint64_t size) : ptr_{ptr}, size_{size} {}
+
+  /// Constructs a @c tapa::mmap from the given @c std::vector.
+  ///
+  /// @param vec @c std::vector being mapped.
   template <typename Allocator>
   explicit mmap(
       std::vector<typename std::remove_const<T>::type, Allocator>& vec)
       : ptr_{vec.data()}, size_{vec.size()} {}
+
+  /// Implicitly casts to a regular pointer.
+  ///
+  /// @c tapa::mmap should be used just like a pointer in the kernel.
   operator T*() { return ptr_; }
 
-  // Increment / decrement.
-  T& operator++() { return *++ptr_; }
-  T& operator--() { return *--ptr_; }
-  T operator++(int) { return *ptr_++; }
-  T operator--(int) { return *ptr_--; }
+  /// Increments the start of the mapped memory.
+  ///
+  /// @return The incremented @c tapa::mmap.
+  mmap& operator++() {
+    ++ptr_;
+    return *this;
+  }
 
-  // Convert to async_mmap.
-  tapa::async_mmap<T> async_mmap() { return tapa::async_mmap<T>(*this); }
+  /// Decrements the start of the mapped memory.
+  ///
+  /// @return The decremented @c tapa::mmap.
+  mmap& operator--() {
+    --ptr_;
+    return *this;
+  }
 
-  // Host-only APIs.
+  /// Increments the start of the mapped memory.
+  ///
+  /// @return The @c tapa::mmap before incrementation.
+  mmap operator++(int) { return mmap(ptr_++); }
+
+  /// Decrements the start of the mapped memory.
+  ///
+  /// @return The @c tapa::mmap before decrementation.
+  mmap operator--(int) { return mmap(ptr_--); }
+
+  /// Retrieves the start of the mapped memory.
+  ///
+  /// This should be used on the host only.
+  ///
+  /// @return The start of the mapped memory.
   T* get() const { return ptr_; }
+
+  /// Retrieves the size of the mapped memory.
+  ///
+  /// This should be used on the host only.
+  ///
+  /// @return The size of the mapped memory (in unit of element count).
   uint64_t size() const { return size_; }
+
+  /// Reinterprets the element type of the mapped memory as
+  /// <tt>tapa::vec_t<T, N></tt>.
+  ///
+  /// This should be used on the host only.
+  /// The size of mapped memory must be a multiple of @c N.
+  ///
+  /// @tparam N  Vector length of the new element type.
+  /// @return @c tapa::mmap of the same piece of memory but of type
+  ///         <tt>tapa::vec_t<T, N></tt>.
   template <uint64_t N>
   mmap<vec_t<T, N>> vectorized() const {
     CHECK_EQ(size_ % N, 0) << "size must be a multiple of N";
@@ -89,15 +144,23 @@ class mmap {
 };
 #endif  // __SYNTHESIS__
 
+/// Defines a view of a piece of consecutive memory with asynchronous random
+/// accesses.
 template <typename T>
 #ifdef __SYNTHESIS__
 // HLS doesn't like non-reference instance of hls::stream in the arguments.
 using async_mmap = internal::async_mmap<T>&;
 #else   // __SYNTHESIS__
 class async_mmap : public mmap<T> {
-  using super = mmap<T>;
+ public:
+  /// Type of the addresses.
   using addr_t = int64_t;
+
+  /// Type of the write responses.
   using resp_t = uint8_t;
+
+ private:
+  using super = mmap<T>;
 
   tapa::stream<addr_t, 64> read_addr_q_{"read_addr"};
   tapa::stream<T, 64> read_data_q_{"read_data"};
@@ -106,7 +169,13 @@ class async_mmap : public mmap<T> {
   tapa::stream<resp_t, 64> write_resp_q_{"write_resp"};
 
   // Only convert when scheduled.
-  async_mmap(const super& mem) : super(mem) {}
+  async_mmap(const super& mem)
+      : super(mem),
+        read_addr(read_addr_q_),
+        read_data(read_data_q_),
+        write_addr(write_addr_q_),
+        write_data(write_data_q_),
+        write_resp(write_resp_q_) {}
 
   // Must be operated via the read/write addr/data stream APIs.
   operator T*() { return super::ptr_; }
@@ -129,11 +198,35 @@ class async_mmap : public mmap<T> {
   std::ptrdiff_t operator-(async_mmap<T> ptr) { return super::ptr_ - ptr; }
 
  public:
-  tapa::ostream<addr_t> read_addr{read_addr_q_};
-  tapa::istream<T> read_data{read_data_q_};
-  tapa::ostream<addr_t> write_addr{write_addr_q_};
-  tapa::ostream<T> write_data{write_data_q_};
-  tapa::istream<resp_t> write_resp{write_resp_q_};
+  /// Provides access to the <i>read address</i> channel.
+  ///
+  /// Each value written to this channel triggers an asynchronous memory read
+  /// request. Consecutive requests may be coalesced into a long burst request.
+  tapa::ostream<addr_t> read_addr;
+
+  /// Provides access to the read data channel.
+  ///
+  /// Each value read from this channel represents the data retrieved from the
+  /// underlying memory system.
+  tapa::istream<T> read_data;
+
+  /// Provides access to the write address channel.
+  ///
+  /// Each value written to this channel triggers an asynchronous memory write
+  /// request. Consecutive requests may be coalesced into a long burst request.
+  tapa::ostream<addr_t> write_addr;
+
+  /// Provides access to the write data channel.
+  ///
+  /// Each value written to this channel supplies data to the memory write
+  /// request.
+  tapa::ostream<T> write_data;
+
+  /// Provides access to the write response channel.
+  ///
+  /// Each value read from this channel represents the data count acknowledged
+  /// by the underlying memory system.
+  tapa::istream<resp_t> write_resp;
 
   void operator()() {
     int16_t write_count = 0;
@@ -172,6 +265,7 @@ class async_mmap : public mmap<T> {
 };
 #endif  // __SYNTHESIS__
 
+/// Defines an array of @c tapa::mmap.
 #ifndef __SYNTHESIS__
 template <typename T, uint64_t S>
 class mmaps {
@@ -179,17 +273,29 @@ class mmaps {
   std::vector<mmap<T>> mmaps_;
 
  public:
-  // constructors
+  /// Constructs a @c tapa::mmap array with unknown sizes.
+  ///
+  /// @param ptrs Pointers to the start of the array of mapped memory.
   mmaps(const std::array<T*, S>& ptrs) {
     for (uint64_t i = 0; i < S; ++i) {
       mmaps_.emplace_back(ptrs[i]);
     }
   }
+
+  /// Constructs a @c tapa::mmap array from an @c std::array of pointers and an
+  /// @c std::array of sizes.
+  ///
+  /// @param ptrs  Pointers to the start of the array of mapped memory.
+  /// @param sizes Sizes of each mapped memory (in unit of element count).
   mmaps(const std::array<T*, S>& ptrs, const std::array<uint64_t, S>& sizes) {
     for (uint64_t i = 0; i < S; ++i) {
       mmaps_.emplace_back(ptrs[i], sizes[i]);
     }
   }
+
+  /// Constructs a @c tapa::mmap array from an @c std::vector array.
+  ///
+  /// @param vecs @c std::vector array being mapped.
   template <typename Allocator>
   explicit mmaps(
       std::vector<typename std::remove_const<T>::type, Allocator> (&vecs)[S]) {
@@ -197,6 +303,10 @@ class mmaps {
       mmaps_.emplace_back(vecs[i]);
     }
   }
+
+  /// Constructs a @c tapa::mmap array from an @c std::array of @c std::vector.
+  ///
+  /// @param vecs @c std::vector array being mapped.
   template <typename Allocator>
   explicit mmaps(
       std::array<std::vector<typename std::remove_const<T>::type, Allocator>,
@@ -215,6 +325,7 @@ class mmaps {
   // default move assignment operator
   mmaps& operator=(mmaps&&) = default;
 
+  /// References a @c tapa::mmap in the array.
   mmap<T>& operator[](int idx) { return mmaps_[idx]; };
 
   template <uint64_t offset, uint64_t length>
@@ -227,7 +338,15 @@ class mmaps {
     return result;
   }
 
-  // Host-only APIs.
+  /// Reinterprets the element type of each mapped memory as
+  /// <tt>tapa::vec_t<T, N></tt>.
+  ///
+  /// This should be used on the host only.
+  /// The size of each mapped memory must be a multiple of @c N.
+  ///
+  /// @tparam N  Vector length of the new element type.
+  /// @return @c tapa::mmap of the same pieces of memory but of type
+  ///         <tt>tapa::vec_t<T, N></tt>.
   template <uint64_t N>
   mmaps<vec_t<T, N>, S> vectorized() const {
     std::array<vec_t<T, N>*, S> ptrs;
