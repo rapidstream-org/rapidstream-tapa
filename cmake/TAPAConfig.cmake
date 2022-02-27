44b96099be4962d6718e20d23b3c6cce3f4b0580
Blaok <i@blaok.me>
2022-02-26 17:59:54 -0800

    build: support vendor include paths in cmake

--- cmake/TAPAConfig.cmake
+++ cmake/TAPAConfig.cmake
@@ -3,3 +3,11 @@ find_package(FRT REQUIRED)
 
 include("${CMAKE_CURRENT_LIST_DIR}/TAPATargets.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/TAPACCConfig.cmake")
+
+if(EXISTS "$ENV{XILINX_HLS}/include")
+  set_target_properties(
+    tapa::tapa tapa::tapa_shared
+    PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "$ENV{XILINX_HLS}/include"
+  )
+  message(STATUS "Using XILINX_HLS include path: $ENV{XILINX_HLS}/include")
+endif()
--- cmake/apps.cmake
+++ cmake/apps.cmake
@@ -3,6 +3,11 @@ if(PROJECT_NAME STREQUAL "tapa")
   set(TAPAC PYTHONPATH=${CMAKE_SOURCE_DIR}/backend/python python3 -m tapa.tapac
             --cflags=-I${CMAKE_SOURCE_DIR}/src)
   set(TAPACC $<TARGET_FILE:tapacc>)
+
+  if(EXISTS "$ENV{XILINX_HLS}/include")
+    include_directories(SYSTEM "$ENV{XILINX_HLS}/include")
+    message(STATUS "Using XILINX_HLS include path: $ENV{XILINX_HLS}/include")
+  endif()
 else()
   find_package(TAPA REQUIRED)
   find_package(FRT)
