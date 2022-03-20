b7fee01210b32a5bf704d46efd597e308fd04a7c
Blaok <i@blaok.me>
2022-03-19 17:10:21 -0700

    ci: remove indirect dependencies

--- .github/scripts/setup-self-hosted.sh
+++ .github/scripts/setup-self-hosted.sh
@@ -3,15 +3,7 @@ set -e
 
 sudo apt-get update
 sudo apt-get install -y --no-install-recommends \
-  faketime \
-  gcc-multilib \
   iverilog \
-  libc6-dev-i386 \
-  libncurses5 \
-  libtinfo5 \
-  libxext6 \
-  unzip \
-  xrt
 
 # Vivado doesn't like running concurrently for the first time.
 source "${XILINX_VIVADO}"/settings64.sh
