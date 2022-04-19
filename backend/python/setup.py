b341eb0321b9399c5f4f49d3ab24285ff60133a0
Licheng-Guo <lichengguo777@gmail.com>
2022-04-19 16:19:16 -0700

    build: bump autobridge version

--- backend/python/setup.py
+++ backend/python/setup.py
@@ -36,7 +36,7 @@ setup(
     packages=find_packages(),
     python_requires='>=3.6',
     install_requires=[
-        'autobridge>=0.0.20220413.dev2',
+        'autobridge>=0.0.20220420.dev2',
         'haoda>=0.0.20220410.dev1',
         'pyverilog>=1.2.0',
         'pyyaml>=5.1',
