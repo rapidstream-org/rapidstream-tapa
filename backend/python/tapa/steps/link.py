68b6c7677e90194d9a138dc93162739f174890c8
Jason Lau <lau@cs.ucla.edu>
2022-05-03 16:53:27 -0700

    feat(tapa): new cli tapa link

--- backend/python/tapa/steps/link.py
+++ backend/python/tapa/steps/link.py
@@ -1,6 +1,44 @@
+import os
+
 import click
+
+import tapa.core
 
 
 @click.command()
-def link():
-  pass
+@click.pass_context
+@click.option(
+    '--floorplan-output',
+    type=click.Path(dir_okay=False, writable=True),
+    help='Specify the output tcl file that encodes the floorplan results. '
+    'Defaults to [work-dir]/constraints.tcl',
+)
+@click.option(
+    '--register-level',
+    type=int,
+    default=0,
+    help='Use a specific register level of top-level scalar signals '
+    'instead of inferring from the floorplanning directive.',
+)
+def link(ctx, floorplan_output: str, register_level: int):
+
+  work_dir: str
+  work_dir = ctx.obj['work-dir']
+
+  program: tapa.core.Program
+  program = ctx.obj.get('program', None)
+  if program is None:
+    raise click.BadArgumentUsage('Link commands must be chained after synth.')
+
+  floorplan_output_file = None
+  if ctx.obj.get('floorplanned', False):
+    if floorplan_output is None:
+      floorplan_output = os.path.join(work_dir, 'constraints.tcl')
+    floorplan_output_file = open(floorplan_output, 'w')
+
+  program.generate_top_rtl(
+      floorplan_output_file,
+      register_level,
+      ctx.obj['additional-fifo-pipelining'],
+      ctx.obj['part-num'],
+  )
