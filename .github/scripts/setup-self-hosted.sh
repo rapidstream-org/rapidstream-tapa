c8567ed8aff871257a13d2621e7605ec859e231f
Blaok <i@blaok.me>
2022-05-04 21:19:14 -0700

    ci(CI): do not install python part as editable

--- .github/scripts/setup-self-hosted.sh
+++ .github/scripts/setup-self-hosted.sh
@@ -4,6 +4,7 @@ set -e
 sudo apt-get update
 sudo apt-get install -y --no-install-recommends \
   iverilog \
+  python3-venv \
 
 # Vivado doesn't like running concurrently for the first time.
 source "${XILINX_VIVADO}"/settings64.sh
