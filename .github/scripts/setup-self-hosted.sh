1974a76055e65e880ad6f94193021acbebb81fe8
Blaok <i@blaok.me>
2022-05-24 21:47:07 -0700

    ci(CI): reuse built packages

--- .github/scripts/setup-self-hosted.sh
+++ .github/scripts/setup-self-hosted.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -e
 
+wget -O- https://raw.github.com/Blaok/fpga-runtime/master/install.sh | bash
+
 sudo apt-get update
 sudo apt-get install -y --no-install-recommends \
-  iverilog \
-  python3-venv \
+  cmake \
+  moreutils
+sudo apt-get purge -y \
+  parallel
 
 # Vivado doesn't like running concurrently for the first time.
 source "${XILINX_VIVADO}"/settings64.sh
