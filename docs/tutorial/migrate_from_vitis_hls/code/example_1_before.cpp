4ae9af2ec2d10a40f419db1caaee45deacfd7b2c
Blaok <i@blaok.me>
2022-06-29 23:30:45 -0700

    refactor: format docs/tutorial/migrate_from_vitis_hls/code/example_1_before.cpp

--- docs/tutorial/migrate_from_vitis_hls/code/example_1_before.cpp
+++ docs/tutorial/migrate_from_vitis_hls/code/example_1_before.cpp
@@ -1,6 +1,6 @@
 
-#include <hls_vector.h>
 #include <hls_stream.h>
+#include <hls_vector.h>
 #include "assert.h"
 
 #define MEMORY_DWIDTH 512
@@ -9,61 +9,51 @@
 
 #define DATA_SIZE 4096
 
-void load_input(
-    hls::vector<uint32_t, NUM_WORDS>* in,
-    hls::stream<hls::vector<uint32_t, NUM_WORDS>>& inStream,
-    int Size
-) {
+void load_input(hls::vector<uint32_t, NUM_WORDS>* in,
+                hls::stream<hls::vector<uint32_t, NUM_WORDS>>& inStream,
+                int Size) {
   for (int i = 0; i < Size; i++) {
-  #pragma HLS pipeline II=1
+#pragma HLS pipeline II = 1
     inStream << in[i];
   }
 }
 
-void compute_add(
-    hls::stream<hls::vector<uint32_t, NUM_WORDS>>& in1_stream,
-    hls::stream<hls::vector<uint32_t, NUM_WORDS>>& in2_stream,
-    hls::stream<hls::vector<uint32_t, NUM_WORDS>>& out_stream,
-    int Size
-) {
+void compute_add(hls::stream<hls::vector<uint32_t, NUM_WORDS>>& in1_stream,
+                 hls::stream<hls::vector<uint32_t, NUM_WORDS>>& in2_stream,
+                 hls::stream<hls::vector<uint32_t, NUM_WORDS>>& out_stream,
+                 int Size) {
   for (int i = 0; i < Size; i++) {
-  #pragma HLS pipeline II=1
+#pragma HLS pipeline II = 1
     out_stream << (in1_stream.read() + in2_stream.read());
   }
 }
 
-void store_result(
-    hls::vector<uint32_t, NUM_WORDS>* out,
-    hls::stream<hls::vector<uint32_t, NUM_WORDS>>& out_stream,
-    int Size
-) {
+void store_result(hls::vector<uint32_t, NUM_WORDS>* out,
+                  hls::stream<hls::vector<uint32_t, NUM_WORDS>>& out_stream,
+                  int Size) {
   for (int i = 0; i < Size; i++) {
-  #pragma HLS pipeline II=1
+#pragma HLS pipeline II = 1
     out[i] = out_stream.read();
   }
 }
 
 extern "C" {
 
-void vadd(
-    hls::vector<uint32_t, NUM_WORDS>* in1,
-    hls::vector<uint32_t, NUM_WORDS>* in2,
-    hls::vector<uint32_t, NUM_WORDS>* out,
-    int size
-) {
-  #pragma HLS INTERFACE m_axi port = in1 bundle = gmem0
-  #pragma HLS INTERFACE m_axi port = in2 bundle = gmem1
-  #pragma HLS INTERFACE m_axi port = out bundle = gmem0
+void vadd(hls::vector<uint32_t, NUM_WORDS>* in1,
+          hls::vector<uint32_t, NUM_WORDS>* in2,
+          hls::vector<uint32_t, NUM_WORDS>* out, int size) {
+#pragma HLS INTERFACE m_axi port = in1 bundle = gmem0
+#pragma HLS INTERFACE m_axi port = in2 bundle = gmem1
+#pragma HLS INTERFACE m_axi port = out bundle = gmem0
 
   hls::stream<hls::vector<uint32_t, NUM_WORDS>> in1_stream("input_stream_1");
   hls::stream<hls::vector<uint32_t, NUM_WORDS>> in2_stream("input_stream_2");
   hls::stream<hls::vector<uint32_t, NUM_WORDS>> out_stream("output_stream");
 
-  #pragma HLS dataflow
+#pragma HLS dataflow
   load_input(in1, in1_stream, size);
   load_input(in2, in2_stream, size);
   compute_add(in1_stream, in2_stream, out_stream, size);
   store_result(out, out_stream, size);
 }
-
 }
