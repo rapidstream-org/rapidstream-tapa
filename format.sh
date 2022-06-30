20f4aa3445e1858bfa89badded459028935cef1a
Blaok <i@blaok.me>
2022-06-29 23:27:33 -0700

    ci(lint): add formatting script

---
+++ format.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Requires: clang-format
+
+set -e
+
+cd "${0%/*}"
+
+find \( \
+  -path './backend/clang' -or \
+  -path './backend/python/tapa/assets/clang' -or \
+  -path './regression' -or \
+  -path '*/build/*' \
+  \) -prune -or \( \
+  -iname '*.h' -or -iname '*.cpp' \
+  \) -print0 | xargs --null clang-format -i
