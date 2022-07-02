903598f9f67ad14068ae26bfd993558fbcfb5fa1
Blaok <i@blaok.me>
2022-07-02 10:30:47 -0700

    ci(lint): check Python formatting

--- format.sh
+++ format.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-# Requires: clang-format
+# Requires: clang-format, yapf, isort
 
 set -e
 
 cd "${0%/*}"
 
+which clang-format
 find \( \
   -path './backend/clang' -or \
   -path './backend/python/tapa/assets/clang' -or \
@@ -13,3 +14,18 @@ find \( \
   \) -prune -or \( \
   -iname '*.h' -or -iname '*.cpp' \
   \) -print0 | xargs --null clang-format -i --verbose
+
+tmp="$(mktemp --suffix=tapa-formatter)"
+trap 'rm -f "${tmp}"' EXIT
+
+find \( \
+  -path './backend/python/tapa/verilog/axi_xbar.py' -or \
+  -path './regression' -or \
+  -path '*/build/*' \
+  \) -prune -or \( \
+  -iname '*.py' \
+  \) -print0 >"${tmp}"
+which yapf
+xargs --null yapf --in-place --verbose <"${tmp}"
+which isort
+xargs --null isort <"${tmp}"
