8559a8cd24f0f1275d9086cf2f02a7f353f997e4
Blaok <i@blaok.me>
2022-11-13 11:47:06 -0800

    test(bandwidth): clean up outdated CMakeLists.txt

--- apps/bandwidth/slr.tcl
+++
@@ -1,5 +0,0 @@
-puts "applying slr.tcl"
-add_cells_to_pblock [get_pblocks pblock_dynamic_SLR0] [get_cells -hierarchical { Copy_0 chan_0__m_axi Bandwidth_control_s_axi_U }]
-add_cells_to_pblock [get_pblocks pblock_dynamic_SLR1] [get_cells -hierarchical { Copy_1 chan_1__m_axi }]
-add_cells_to_pblock [get_pblocks pblock_dynamic_SLR2] [get_cells -hierarchical { Copy_2 chan_2__m_axi }]
-add_cells_to_pblock [get_pblocks pblock_dynamic_SLR3] [get_cells -hierarchical { Copy_3 chan_3__m_axi }]
