a2bd5483e93f7b7138b7735535fc24dbbda496b8
Blaok <i@blaok.me>
2023-12-31 20:32:36 +0000

    refactor: let IWYU export symbols from tapa.h

--- src/tapa.h
+++ src/tapa.h
@@ -3,14 +3,14 @@
 
 #if !defined(TAPA_TARGET_)
 
-#include "tapa/host/tapa.h"
+#include "tapa/host/tapa.h"  // IWYU pragma: export
 
 #elif TAPA_TARGET_ == XILINX_HLS
 
-#include "tapa/xilinx/hls/tapa.h"
+#include "tapa/xilinx/hls/tapa.h"  // IWYU pragma: export
 
 #endif
 
-#include "tapa/traits.h"
+#include "tapa/traits.h"  // IWYU pragma: export
 
 #endif  // TAPA_TAPA_H_
