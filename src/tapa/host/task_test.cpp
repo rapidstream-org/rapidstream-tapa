1b729379405942680f251b68f37c639d3eef0505
Blaok <i@blaok.me>
2024-08-02 22:37:18 -0700

    test(libtapa): add more non-synthesizable `TaskTest`s

--- src/tapa/host/task_test.cpp
+++ src/tapa/host/task_test.cpp
@@ -1,31 +1,55 @@
+#include "tapa/host/task.h"
+
 #include <thread>
 
 #include <gtest/gtest.h>
 
-#include "tapa/host/stream.h"
+#include "tapa.h"
 
 namespace tapa {
 namespace {
 
-void DataSource(tapa::ostream<int> data_out_q, int n) {
+constexpr int kN = 5000;
+
+void DataSource(tapa::ostream<int>& data_out_q, int n) {
   for (int i = 0; i < n; ++i) {
     data_out_q.write(i);
   }
 }
 
-void DataSink(tapa::istream<int> data_in_q, int n) {
+template <typename T>
+void DataSinkTemplated(tapa::istream<T>& data_in_q, int n) {
   for (int i = 0; i < n; ++i) {
     data_in_q.read();
   }
 }
 
+void DataSink(tapa::istream<int>& data_in_q, int n) {
+  DataSinkTemplated(data_in_q, n);
+}
+
+// WARNING: This is not synthesizable.
 TEST(TaskTest, YieldingWithoutCoroutineWorks) {
-  constexpr int kN = 5000;
   tapa::stream<int, 2> data_q;
-  std::thread t1(DataSource, data_q, kN);
-  std::thread t2(DataSink, data_q, kN);
+  std::thread t1(DataSource, std::ref(data_q), kN);
+  std::thread t2(DataSink, std::ref(data_q), kN);
   t1.join();
   t2.join();
+}
+
+// WARNING: This is not synthesizable.
+TEST(TaskTest, DirectInvocationMixedWithTapaInvocationWorks) {
+  tapa::stream<int, kN> data_q;
+  DataSource(data_q, kN);
+  tapa::task().invoke(DataSink, data_q, kN);
+}
+
+// WARNING: This is not synthesizable (yet).
+TEST(TaskTest, InvokingTemplatedTaskWorks) {
+  tapa::stream<int, kN> data_q;
+  tapa::task()
+      .invoke(DataSinkTemplated<int>, data_q, kN)
+      .invoke(DataSource, data_q, kN);
 }
 
 }  // namespace
