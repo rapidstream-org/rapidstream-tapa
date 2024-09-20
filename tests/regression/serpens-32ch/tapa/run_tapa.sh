9f71587195f801e619c7f030d72ab8d2f975da32
RipperJ <1628079212@qq.com>
2024-09-20 21:18:59 +0800

    feat(serpens-32ch): add new tapa script to serpens 32-channel version

---
+++ tests/regression/serpens-32ch/tapa/run_tapa.sh
@@ -0,0 +1,15 @@
+# Copyright (c) 2024 RapidStream Design Automation, Inc. and contributors.
+# All rights reserved. The contributor(s) of this file has/have agreed to the
+# RapidStream Contributor License Agreement.
+
+WORK_DIR=generated
+
+tapa \
+  --work-dir ${WORK_DIR} \
+  compile \
+  --top serpens \
+  --part-num xcu280-fsvh2892-2L-e \
+  --clock-period 3.33 \
+  -o ${WORK_DIR}/serpens32.xo \
+  -f src/serpens_tapa.cpp \
+  2>&1 | tee ${WORK_DIR}/tapa.log
