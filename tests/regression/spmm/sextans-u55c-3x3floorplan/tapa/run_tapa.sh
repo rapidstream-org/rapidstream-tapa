8b66297d293e2bfad2b8d723e1f6538690d6c6e0
RipperJ <linfeng.du@rapidstream-da.com>
2024-09-19 23:50:46 -0700

    feat(rsdemo): add sextans 3x3 280MHz demo

---
+++ tests/regression/spmm/sextans-u55c-3x3floorplan/tapa/run_tapa.sh
@@ -0,0 +1,15 @@
+# Copyright (c) 2024 RapidStream Design Automation, Inc. and contributors.
+# All rights reserved. The contributor(s) of this file has/have agreed to the
+# RapidStream Contributor License Agreement.
+
+WORK_DIR=generated
+
+tapa \
+  --work-dir ${WORK_DIR} \
+  compile \
+  --top Sextans \
+  --part-num xcu55c-fsvh2892-2L-e \
+  --clock-period 3.33 \
+  -o ${WORK_DIR}/Sextans1613.xo \
+  -f src/sextans-no-const-para.cpp \
+  2>&1 | tee ${WORK_DIR}/tapa.log
