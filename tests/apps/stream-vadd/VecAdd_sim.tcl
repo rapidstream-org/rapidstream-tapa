d8ee042ec0ded88f5d1d096a1dc8fd0934ed06a3
Jason Lau <lau@rapidstream-da.com>
2024-10-21 13:37:07 -0700

    chore: add copyright notice

--- tests/apps/stream-vadd/VecAdd_sim.tcl
+++ tests/apps/stream-vadd/VecAdd_sim.tcl
@@ -1,3 +1,7 @@
+# Copyright (c) 2024 RapidStream Design Automation, Inc. and contributors.
+# All rights reserved. The contributor(s) of this file has/have agreed to the
+# RapidStream Contributor License Agreement.
+
 set work_dir $argv
 set proj_name "vecadd_proj"
 set proj_dir "$work_dir/vivado_proj"
--- tests/apps/stream-vadd/VecAdd_tb.sv
+++ tests/apps/stream-vadd/VecAdd_tb.sv
@@ -1,5 +1,9 @@
 `timescale 1ns / 1ps
 
+// Copyright (c) 2024 RapidStream Design Automation, Inc. and contributors.
+// All rights reserved. The contributor(s) of this file has/have agreed to the
+// RapidStream Contributor License Agreement.
+
 module VecAdd_tb();
   parameter CLK_PERIOD = 10;
 
