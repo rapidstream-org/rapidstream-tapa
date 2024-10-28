74ef0de08c931fdf57c8b8b82e2bd6004ea1d2c1
Blaok <i@blaok.me>
2024-10-27 22:55:31 -0700

    feat(frt): add DPI device interface

---
+++ bazel/dpi_rules.bzl
@@ -0,0 +1,87 @@
+"""Custom rule to create DPI libraries."""
+
+# Copyright (c) 2024 RapidStream Design Automation, Inc. and contributors.
+# All rights reserved. The contributor(s) of this file has/have agreed to the
+# RapidStream Contributor License Agreement.
+
+def _dpi_library_impl(ctx):
+    compile_options = [
+        "-Wall",
+        "-Werror",
+        "-Wno-sign-compare",
+    ]
+    transitive_inputs = []
+    compile_options += ["-isystem" + x.path for x in ctx.files.includes]
+    for dep in ctx.attr.deps:
+        context = dep[CcInfo].compilation_context
+        compile_options += ["-iquote" + x for x in context.quote_includes.to_list()]
+        compile_options += ["-isystem" + x for x in context.system_includes.to_list()]
+        compile_options += ["-I" + x for x in context.includes.to_list()]
+        compile_options += ["-D" + x for x in context.defines.to_list()]
+        transitive_inputs.append(context.headers)
+
+    link_options = [
+        # Prefer libraries in the same directory. Escaping '$' due to `xsc` bug.
+        "-Wl,-rpath,\\$ORIGIN",
+    ]
+
+    # Link against the dynamic libraries in `deps`.
+    direct_inputs = ctx.files.hdrs + ctx.files.srcs
+    for dep in ctx.attr.deps:
+        for linker_input in dep[CcInfo].linking_context.linker_inputs.to_list():
+            for library in linker_input.libraries:
+                dynamic_library = library.resolved_symlink_dynamic_library
+                if dynamic_library == None:
+                    dynamic_library = library.dynamic_library
+                if dynamic_library == None:
+                    continue
+                direct_inputs.append(dynamic_library)
+                name = dynamic_library.basename
+                name = name.removeprefix("lib")
+                name = name.removesuffix("." + dynamic_library.extension)
+                link_options += [
+                    "-L" + dynamic_library.dirname,
+                    "-l" + name,
+                    "-Wl,-rpath," + dynamic_library.dirname,
+                ]
+
+    # Assemble arguments.
+    output = ctx.actions.declare_file(ctx.label.name + ".so")
+    args = [
+        "--output=" + output.path,
+        "--mt=off",
+    ]
+    args += ["--gcc_compile_options=" + x for x in compile_options]
+    args += ["--gcc_link_options=" + x for x in link_options]
+    args += [x.path for x in ctx.files.srcs]
+
+    ctx.actions.run(
+        outputs = [output],
+        inputs = depset(direct_inputs, transitive = transitive_inputs),
+        executable = ctx.executable._xsc,
+        tools = [ctx.executable._xsc],
+        arguments = args,
+        mnemonic = "DpiCompile",
+    )
+    return [
+        DefaultInfo(files = depset([output])),
+    ]
+
+_dpi_library = rule(
+    implementation = _dpi_library_impl,
+    attrs = {
+        "srcs": attr.label_list(allow_files = True),
+        "hdrs": attr.label_list(allow_files = True),
+        "includes": attr.label_list(allow_files = True),
+        "deps": attr.label_list(providers = [CcInfo]),
+        "_xsc": attr.label(
+            cfg = "exec",
+            default = Label("//bazel:xsc"),
+            executable = True,
+        ),
+    },
+)
+
+def dpi_library(name, **kwargs):
+    _dpi_library(name = name, **kwargs)
+    native.cc_library(name = name + "_cc", **kwargs)
---
+++ fpga-runtime/src/frt/devices/tapa_fast_cosim_dpi.cpp
@@ -0,0 +1,165 @@
+// Copyright (c) 2024 RapidStream Design Automation, Inc. and contributors.
+// All rights reserved. The contributor(s) of this file has/have agreed to the
+// RapidStream Contributor License Agreement.
+
+#include <sstream>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <vector>
+
+#include <fcntl.h>
+
+#include <glog/logging.h>
+#include <svdpi.h>
+
+#include "frt/devices/shared_memory_queue.h"
+
+namespace {
+
+using ::fpga::internal::SharedMemoryQueue;
+
+std::unordered_map<std::string, SharedMemoryQueue::UniquePtr> InitStreamMap() {
+  const char* env = std::getenv("TAPA_FAST_COSIM_DPI_ARGS");
+  CHECK(env != nullptr) << "Please set `TAPA_FAST_COSIM_DPI_ARGS`";
+  VLOG(1) << "TAPA_FAST_COSIM_DPI_ARGS: " << env;
+
+  std::vector<std::tuple<std::string, std::string>> stream_id_and_path;
+  {
+    std::istringstream ss(env);
+    std::string stream_entry;  // `id:path`
+    while (std::getline(ss, stream_entry, /*delimiter=*/',')) {
+      const std::string::size_type pos = stream_entry.find(':');
+      CHECK_NE(pos, std::string::npos) << stream_entry;
+      stream_id_and_path.push_back({
+          stream_entry.substr(0, pos),
+          stream_entry.substr(pos + 1),
+      });
+    }
+  }
+
+  std::unordered_map<std::string, SharedMemoryQueue::UniquePtr> streams;
+  for (const std::tuple<std::string, std::string>& entry : stream_id_and_path) {
+    const std::string& stream_id = std::get<0>(entry);
+    const std::string& stream_path = std::get<1>(entry);
+    int fd = open(stream_path.c_str(), O_RDWR | O_CLOEXEC);
+    VLOG(2) << "fd: " << fd << " <=> arg: " << stream_id;
+    streams[stream_id] = SharedMemoryQueue::New(fd);
+  }
+  return streams;
+}
+
+SharedMemoryQueue* GetStream(const char* id) {
+  if (id == nullptr) {
+    LOG(ERROR) << "stream id is nullptr";
+    return nullptr;
+  }
+  static std::unordered_map<std::string, SharedMemoryQueue::UniquePtr> streams =
+      InitStreamMap();
+  auto it = streams.find(id);
+  if (it == streams.end()) {
+    return nullptr;
+  }
+  return it->second.get();
+}
+
+void StringToOpenArrayHandle(const std::string& bits,
+                             svOpenArrayHandle handle) {
+  const int increment = svIncrement(handle, 1);
+  int index = svLeft(handle, 1);
+  for (const char c : bits) {
+    svLogic bit = sv_x;
+    switch (c) {
+      case '0':
+        bit = sv_0;
+        break;
+      case '1':
+        bit = sv_1;
+        break;
+      case 'z':
+      case 'Z':
+        bit = sv_z;
+        break;
+      case 'x':
+      case 'X':
+        bit = sv_x;
+        break;
+      case '\'':
+      case '_':
+        continue;
+      default:
+        LOG(FATAL) << "unexpected bit character: " << c << " (" << int(c)
+                   << ")";
+    }
+    svPutLogicArrElem(handle, bit, index);
+    index -= increment;
+  }
+  CHECK_EQ(index, svRight(handle, 1) - increment);
+}
+
+std::string OpenArrayHandleToString(svOpenArrayHandle handle) {
+  std::string bits;
+  bits.reserve(svSize(handle, 1));
+  const int increment = svIncrement(handle, 1);
+  for (int index = svLeft(handle, 1); index != svRight(handle, 1) - increment;
+       index -= increment) {
+    bits.push_back([&] {
+      const svLogic bit = svGetLogicArrElem(handle, index);
+      switch (bit) {
+        case sv_0:
+          return '0';
+        case sv_1:
+          return '1';
+        case sv_x:
+          return 'x';
+        case sv_z:
+          return 'z';
+        default:
+          LOG(FATAL) << "unexpected bit enum: " << int(bit);
+          return 'x';
+      }
+    }());
+  }
+  return bits;
+}
+
+}  // namespace
+
+extern "C" {
+
+DPI_DLLESPEC void istream(
+    /* output */ svOpenArrayHandle dout,
+    /* output */ svLogic& empty_n,
+    /* input */ svLogic read, int time,
+    /* input */ const char* id) {
+  SharedMemoryQueue* istream = GetStream(id);
+  CHECK(istream != nullptr);
+  CHECK_EQ(istream->width(), svSize(dout, 1));
+  empty_n = istream->empty() ? sv_0 : sv_1;
+  std::string bits(istream->width(), 'x');
+  if (!istream->empty()) {
+    bits = istream->front();
+  }
+  StringToOpenArrayHandle(bits, dout);
+  if (read == sv_1 && !istream->empty()) {
+    istream->pop();
+  }
+}
+
+DPI_DLLESPEC void ostream(
+    /* input */ svOpenArrayHandle din,
+    /* output */ svLogic& full_n,
+    /* input */ svLogic write,
+    /* input */ const char* id) {
+  SharedMemoryQueue* ostream = GetStream(id);
+  CHECK(ostream != nullptr);
+  CHECK_EQ(ostream->width(), svSize(din, 1));
+  if (write == sv_1) {
+    CHECK(!ostream->full());
+    const std::string bits = OpenArrayHandleToString(din);
+    ostream->push(bits);
+  }
+  full_n = (ostream->size() < ostream->capacity()) ? sv_1 : sv_0;
+}
+
+}  // extern "C"
