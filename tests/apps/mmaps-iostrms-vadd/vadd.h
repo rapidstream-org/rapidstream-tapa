fec032e6e71dc3ef41672ab819acfc5229f8e28e
Ed-5100 <yx388@rapidstrea-da.com>
2024-10-29 19:31:18 -0400

    test: added mmaps iostreams test

---
+++ tests/apps/mmaps-iostrms-vadd/vadd.h
@@ -0,0 +1,7 @@
+// Copyright (c) 2024 RapidStream Design Automation, Inc. and contributors.
+// All rights reserved. The contributor(s) of this file has/have agreed to the
+// RapidStream Contributor License Agreement.
+
+#include <tapa.h>
+
+constexpr int M = 4;
