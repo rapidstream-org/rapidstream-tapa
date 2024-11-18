1abe3470f93cc0e0058fa356aa2beecf9b42e4e2
RipperJ <linfeng.du@rapidstream-da.com>
2024-11-17 20:45:16 -0800

    feat(reg): add one-click script for mm-u250 regression

--- tests/regression/autosa/mm/10x13/u250/rapidstream/run_rs.sh
+++ tests/regression/autosa/mm/10x13/u250/rapidstream/run_rs.sh
@@ -2,10 +2,29 @@
 # All rights reserved. The contributor(s) of this file has/have agreed to the
 # RapidStream Contributor License Agreement.
 
+# Tapa Rapidstream running script
+WORK_DIR=../tapa/generated/
+XO_FILE=10x13-u250.xo
+
+# Generate configuration files
+python ./gen_config.py
+
+# Synthesizing the app to generate the XO file
+tapa \
+  --work-dir ${WORK_DIR} \
+  compile \
+  --top kernel0 \
+  --part-num xcu250-figd2104-2L-e \
+  --clock-period 3.00 \
+  -f ../tapa/src/kernel_kernel.cpp \
+  -o ${WORK_DIR}/${XO_FILE} \
+  2>&1 | tee ${WORK_DIR}/tapa.log \
+
+# Optimizing the design with RapidStream
 rapidstream-tapaopt \
     --work-dir run \
-    --tapa-xo-path ./10x13-u250.xo \
-    --device-config ./u250_device.json \
+    --tapa-xo-path ${WORK_DIR}/${XO_FILE} \
+    --device-config ./device_config.json \
     --floorplan-config ./floorplan_config.json \
     --connectivity-ini ./link_config.ini \
     --implementation-config ./impl_config.json \
