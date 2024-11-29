17926c6ce05bb49c0f1d37e5dc0d4256fe61e3bc
Jason Lau <lau@rapidstream-da.com>
2024-11-28 19:26:51 -0800

    test(functional): leaf task stream

---
+++ tests/functional/leaf-task-stream/vadd.cpp
@@ -0,0 +1,61 @@
+// Copyright (c) 2024 RapidStream Design Automation, Inc. and contributors.
+// All rights reserved. The contributor(s) of this file has/have agreed to the
+// RapidStream Contributor License Agreement.
+
+#include <cstdint>
+
+#include <tapa.h>
+
+// Leaf task to instantiate a stream as a sliding window.
+void Mmap2Stream(tapa::mmap<const float> mmap, uint64_t n,
+                 tapa::ostream<float>& stream) {
+  tapa::stream<float, 8> intermediate_stream("intermediate_stream");
+// TODO: Remove the pragma
+#pragma HLS STREAM variable = intermediate_stream._ depth = 32
+
+  for (uint64_t i = 0; i < 8; ++i) {
+    intermediate_stream << mmap[i];
+  }
+  for (uint64_t i = 8; i < n; ++i) {
+    stream << intermediate_stream.read();
+    intermediate_stream << mmap[i];
+  }
+  for (uint64_t i = 0; i < 8; ++i) {
+    stream << intermediate_stream.read();
+  }
+}
+
+void Stream2Mmap(tapa::istream<float>& stream, tapa::mmap<float> mmap,
+                 uint64_t n) {
+  for (uint64_t i = 0; i < n; ++i) {
+    stream >> mmap[i];
+  }
+}
+
+void Add(tapa::istream<float>& a, tapa::istream<float>& b,
+         tapa::ostream<float>& c, uint64_t n) {
+  for (uint64_t i = 0; i < n; ++i) {
+    c << (a.read() + b.read());
+  }
+}
+
+// Leaf task to instantiate a stream as a communication channel.
+void Add2Mmap(tapa::istream<float>& a, tapa::istream<float>& b,
+              tapa::mmap<float> c, uint64_t n) {
+#pragma HLS dataflow
+  // TODO: Simulating leaf tasks should be in compat mode
+  tapa::stream<float, 1024> c_q("c");
+  Add(a, b, c_q, n);
+  Stream2Mmap(c_q, c, n);
+}
+
+void VecAdd(tapa::mmap<const float> a, tapa::mmap<const float> b,
+            tapa::mmap<float> c, uint64_t n) {
+  tapa::stream<float> a_q("a");
+  tapa::stream<float> b_q("b");
+
+  tapa::task()
+      .invoke(Mmap2Stream, a, n, a_q)
+      .invoke(Mmap2Stream, b, n, b_q)
+      .invoke(Add2Mmap, a_q, b_q, c, n);
+}
