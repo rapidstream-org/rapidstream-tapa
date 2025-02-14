3d89907d9238e003253584dd66f103a93af717e9
Ed-5100 <yx388@rapidstrea-da.com>
2025-02-13 20:06:33 -0500

    feat: generate grouping constraint given nonpipeline stream

--- tapa/steps/synth.py
+++ tapa/steps/synth.py
@@ -6,6 +6,9 @@ All rights reserved. The contributor(s) of this file has/have agreed to the
 RapidStream Contributor License Agreement.
 """
 
+import json
+import os
+from pathlib import Path
 from typing import NoReturn
 
 import click
@@ -79,6 +82,16 @@ from tapa.steps.common import (
     default="hls",
     help="Flow Option: 'hls' for FPGA Fabric steps, 'aie' for Versal AIE steps.",
 )
+@click.option(
+    "--nonpipeline-fifos",
+    type=click.Path(dir_okay=False, writable=True),
+    default=None,
+    help=(
+        "`--nonpipeline_fifos` specifies the stream FIFOs to not add "
+        "pipeline to. A grouping_constrains.json file will be generated "
+        "for rapidstream."
+    ),
+)
 def synth(  # noqa: PLR0913,PLR0917
     part_num: str | None,
     platform: str | None,
@@ -90,6 +103,7 @@ def synth(  # noqa: PLR0913,PLR0917
     enable_synth_util: bool,
     print_fifo_ops: bool,
     flow_type: str,
+    nonpipeline_fifos: Path | None,
 ) -> None:
     """Synthesize the TAPA program into RTL code."""
     program = load_tapa_program()
@@ -121,6 +135,17 @@ def synth(  # noqa: PLR0913,PLR0917
         if enable_synth_util:
             program.generate_post_synth_util(part_num, jobs)
         program.generate_top_rtl(print_fifo_ops)
+
+        if nonpipeline_fifos:
+            with open(nonpipeline_fifos, encoding="utf-8") as fifo_file:
+                fifos = json.load(fifo_file)
+            with open(
+                os.path.join(program.work_dir, "grouping_constraints.json"),
+                "w",
+                encoding="utf-8",
+            ) as json_file:
+                result = program.get_grouping_constraints(fifos)
+                json.dump(result, json_file)
 
         settings["synthed"] = True
         store_persistent_context("settings")
