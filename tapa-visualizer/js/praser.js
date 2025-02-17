c03037e2b570c34f0b2409e9d1af1c788fb9ef6f
Crystal-RainSlide <16851802+Crystal-RainSlide@users.noreply.github.com>
2025-02-18 00:02:41 +0800

    feat(gui): combo info, basic node style, cpp code refactor(gui): code structure, add helper.js and sidebar.css fix(gui): sidebar layout & graph layout

--- tapa-visualizer/js/praser.js
+++ tapa-visualizer/js/praser.js
@@ -6,6 +6,10 @@
 
 "use strict";
 
+import { getComboName } from "./helper.js";
+
+/** Color for node with more than 1 connection */
+const majorNodeColor = "#0F5132";
 
 /** @type {<K, V>(map: Map<K, Set<V>>, key: K, value: V) => void} */
 const addToMappedSet = (map, key, value) => {
@@ -13,57 +17,116 @@ const addToMappedSet = (map, key, value) => {
   set ? set.add(value) : map.set(key, new Set([value]));
 };
 
-/** @type {(json: GraphJSON, flat: boolean) => Required<import("@antv/g6").GraphData>} */
+/** [1,2,3,5] -> "1~3,5"
+ * @type {(indexArr: number[]) => string} */
+const getIndexRange = indexes => {
+  // Only 1 index, just return it as string
+  if (indexes.length === 1) return `${indexes[0]}`;
+
+  // More than 1 index, sort & parse to "a~b,c,d~e"
+  indexes.sort((a, b) => a - b);
+
+  // continuous groups of indexs
+  const groups = [[indexes[0]]];
+  for (let i = 1; i < indexes.length; i++) {
+    indexes[i - 1] + 1 === indexes[i]
+    ? groups.at(-1)?.push(indexes[i])
+    : groups.push([indexes[i]]);
+  }
+
+  return groups.map(
+    group => group.length === 1 ? group[0] : `${group[0]}~${group.at(-1)}`
+  ).join(",");
+};
+
+/** @type {(json: GraphJSON, flat: boolean) => GraphData} */
 export const getGraphData = (json, flat = false) => {
 
-  /** @type {Required<import("@antv/g6").GraphData>} */
+  /** @type {GraphData} */
   const graphData = {
     nodes: [],
     edges: [],
     combos: [],
   };
 
+  /** Connection counts for nodes
+   * @type {Map<string, 0 | 1>} */
+  const counts = new Map();
+
+  /** @type {(edge: import("@antv/g6").EdgeData) => void} */
+  const addEdge = (edge) => {
+    [edge.source, edge.target].forEach(id => {
+      if (id.startsWith("combo:")) return;
+      switch (counts.get(id)) {
+        case undefined: // set to false if undefined
+          counts.set(id, 0);
+          break;
+        case 0: { // fill and set to true
+          const node = graphData.nodes.find(node => node.id === id);
+          if (node) node.style = { ...node.style, fill: majorNodeColor };
+          counts.set(id, 1);
+          break;
+        }
+        case 1: // skip filled node
+          break;
+      }
+    });
+    graphData.edges.push(edge);
+  }
+
+  /** Gather upper tasks to determine if there is only 1 upper task
+   * @type {Map<string, UpperTask>} */
+  const upperTasks = new Map();
   for (const taskName in json.tasks) {
-
     const task = json.tasks[taskName];
+    if (task.level === "upper") {
+      upperTasks.set(taskName, task);
+    }
+  }
 
-    // Skip lower tasks
-    if (task.level !== "upper") continue;
+  upperTasks.forEach((upperTask, taskName) => {
 
     // Add upper task as combo
     const combo = `combo:${taskName}`;
     graphData.combos.push({
       id: combo,
-      data: task,
-      type: taskName === json.top ? "rect" : "circle"
+      data: upperTask,
+      // Use rect for top task
+      type: taskName === json.top ? "rect" : "circle",
+      // Use lighter background if there is only 1 upper task
+      style: upperTasks.size > 1 ? {} : { fillOpacity: 0.05 }
     });
 
-    // Add sub task as node
-    for (const subTaskName in task.tasks) {
-      const subTasks = task.tasks[subTaskName];
+    // Add sub-task as node
+    for (const subTaskName in upperTask.tasks) {
+      const subTasks = upperTask.tasks[subTaskName];
+
+      /** @type {UpperTask | LowerTask | undefined} */
+      const task = json.tasks[subTaskName];
+
       if (flat) {
         subTasks.forEach(
           (subTask, i) => graphData.nodes.push({
             id: `${subTaskName}/${i}`,
             combo,
-            data: subTask,
+            data: { task, subTask },
           })
         );
       } else {
         graphData.nodes.push({
           id: subTaskName,
           combo,
-          data: { subTasks },
+          data: { task, subTasks },
         });
       }
     }
 
     /** fifo groups for fifos like fifo_x_xx[0], fifo_x_xx[1]...
-     * @type {Map<string, Set<number> & {source: string, target: string}>} */
+     * @type {Map<string, Set<number>>} */
     const fifoGroups = new Map();
 
-    for (const fifoName in task.fifos) {
-      const fifo = task.fifos[fifoName];
+    for (const fifoName in upperTask.fifos) {
+      const fifo = upperTask.fifos[fifoName];
       if (!fifo.produced_by || !fifo.consumed_by) {
         console.warn(
           `Missing produced_by / consumed_by in ${taskName}'s ${fifoName}:`,
@@ -82,15 +145,11 @@ export const getGraphData = (json, flat = false) => {
       const source = getSubTask(fifo.produced_by);
       const target = getSubTask(fifo.consumed_by);
 
-      // console.log(taskName, fifoName, source, target);
-
-      /** Match fifo groups */
+      // Match fifo groups
       const matchResult = fifoName.match(/^(.*)\[(\d+)\]$/);
       if (matchResult === null) {
-        // Not fifo groups, add edge
-        graphData.edges.push(
-          { source, target, id: `${taskName}/${fifoName}`, data: fifo }
-        );
+        // Not fifo groups, add edge directly
+        addEdge({ source, target, id: `${taskName}/${fifoName}`, data: fifo });
       } else {
         // add fifo group index
         const name = matchResult[1];
@@ -100,30 +159,25 @@ export const getGraphData = (json, flat = false) => {
       }
     }
 
-    fifoGroups.forEach((indexs, key) => {
+    fifoGroups.forEach((indexes, key) => {
       const [name, source, target] = key.split("\n");
-      const indexArr = [...indexs.values()].sort((a, b) => a - b);
-      if (indexArr.some((index, i) => index !== i)) {
-        console.warn(
-          `fifo group: indexes are not continuous at ${taskName}'s ${name}`
-        );
-      }
-      const idWithIndexRange = `${name}[${indexArr[0]}~${indexArr.at(-1)}]`;
-      graphData.edges.push(
-        { source, target, id: `${taskName}/${idWithIndexRange}` }
-      );
-    })
+      const indexRange = getIndexRange([...indexes.values()]);
+      addEdge({ source, target, id: `${taskName}/${name}[${indexRange}]` });
+    });
 
-  }
+  });
 
+  // add combo-to-node connections
+  // TODO: replace this with expand / collapse
   graphData.combos.forEach(combo => {
     if (combo.type === "circle") {
       const node = graphData.nodes.find(
         ({id}) => id.split("/")[0] === getComboName(combo.id)
       );
-      if (node) graphData.edges.push(
-        { source: combo.id, target: node.id, id: `combo-to-node/${node.id}` }
-      );
+      if (node) {
+        const edgeId = `combo-to-node/${node.id}`;
+        addEdge({ source: combo.id, target: node.id, id: edgeId,  });
+      }
     }
   })
 
--- tapa-visualizer/types/g6.d.ts
+++ tapa-visualizer/types/g6.d.ts
@@ -2,18 +2,39 @@
 // All rights reserved. The contributor(s) of this file has/have agreed to the
 // RapidStream Contributor License Agreement.
 
-import _G6, { BaseLayoutOptions, ForceAtlas2LayoutOptions } from "@antv/g6";
-import { BuiltInLayoutOptions as _BuiltInLayoutOptions } from "@antv/g6/lib/layouts/types";
+import g6 from "@antv/g6";
+
+/** Remove index signatures */
+type RemoveIndex<T> = {
+  [
+    K in keyof T as
+    string extends K ? never :
+    number extends K ? never :
+    symbol extends K ? never : K
+  ]: T[K];
+};
 
 declare global {
-  const G6: typeof _G6;
+  const G6: typeof g6; // { [K in keyof typeof g6]: typeof g6[K]; }
+
+  interface NodeData extends Omit<RemoveIndex<g6.NodeData>, "data"> {
+    data:
+    | { task: UpperTask | LowerTask | undefined, subTask: SubTask; }
+    | { task: UpperTask | LowerTask | undefined, subTasks: SubTask[]; };
+  }
+  interface ComboData extends Omit<RemoveIndex<g6.ComboData>, "data"> {
+    data: UpperTask;
+  }
 
   // fix wrong `type` name in @antv/g6/lib/layouts/types.d.ts
-  interface ForceAtlas2 extends BaseLayoutOptions, ForceAtlas2LayoutOptions {
-    type: "forceAtlas2";
-  }
-  interface ForceAtlas2Fixed extends BaseLayoutOptions, ForceAtlas2LayoutOptions {
-    type: "force-atlas2";
-  }
-  type BuiltInLayoutOptions = Exclude<_BuiltInLayoutOptions, ForceAtlas2> | ForceAtlas2Fixed;
+  type BuiltInLayoutOptions =
+    | Exclude<import("@antv/g6/lib/layouts/types").BuiltInLayoutOptions, ForceAtlas2>
+    | ForceAtlas2Fixed;
+}
+
+interface ForceAtlas2 extends g6.BaseLayoutOptions, g6.ForceAtlas2LayoutOptions {
+  type: "forceAtlas2";
+}
+interface ForceAtlas2Fixed extends Omit<g6.BaseLayoutOptions, "type">, g6.ForceAtlas2LayoutOptions {
+  type: "force-atlas2";
 }
