a14b6ebd017f470a70e60a60ba823d493d7a68cf
Crystal-RainSlide <16851802+Crystal-RainSlide@users.noreply.github.com>
2025-03-26 03:14:51 +0800

    feat(gui): lucide loader

--- tapa-visualizer/vite.config.ts
+++ tapa-visualizer/vite.config.ts
@@ -5,17 +5,50 @@
 import type { UserConfig } from "vite";
 import prismjsPlugin from "vite-plugin-prismjs-plus";
 
+import { join } from "node:path";
+import { readFileSync } from "node:fs";
+
 export default {
   build: {
     target: "es2023",
     sourcemap: true,
   },
   plugins: [
+
     prismjsPlugin({
       manual: true,
       css: true,
       languages: ["cpp"],
       plugins: ["line-numbers"],
     }),
+
+    (() => {
+      const name = "vite-plugin-lucide-inline-svg";
+      const lucideStatic = (icon: string) =>
+        `node_modules/lucide-static/icons/${icon}.svg`;
+
+      const minifyLucideSVG = (lucideSVG: string) =>
+        lucideSVG
+          .replace(/<!-- @license lucide-static v[0-9.]+ - ISC -->\n/, "")
+          .replace(/\n\s+/g, " ")
+          .replaceAll("\n", "")
+          .replaceAll("> <", "><")
+          .replaceAll(" />", "/>");
+
+      let root = "";
+
+      return {
+        name,
+        configResolved: config => { root = config.root ?? ""; },
+        // <svg use="x"></svg> -> <svg class="lucide lucide-x" ...>...</svg>
+        transformIndexHtml: html => html.replace(
+          /<svg\s+use="(.+?)"><\/svg>/g,
+          (_match, icon: string) => minifyLucideSVG(
+            readFileSync(join(root, lucideStatic(icon)), "utf-8")
+          ),
+        ),
+      };
+    })(),
+
   ],
 } satisfies UserConfig;
