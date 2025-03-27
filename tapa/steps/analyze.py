919a88c4a18e0bc7aeb5d57fdb03c306321e3514
Ed-5100 <yx388@rapidstrea-da.com>
2025-03-26 20:46:48 -0400

    feat(step): added floorplan option to analyze step

--- tapa/steps/analyze.py
+++ tapa/steps/analyze.py
@@ -1,7 +1,8 @@
+# ruff: noqa: PLR0917,PLR0913
 """Analyze TAPA program and store the program description."""
 
 __copyright__ = """
-Copyright (c) 2024 RapidStream Design Automation, Inc. and contributors.
+Copyright (c) 2025 RapidStream Design Automation, Inc. and contributors.
 All rights reserved. The contributor(s) of this file has/have agreed to the
 RapidStream Contributor License Agreement.
 """
@@ -14,6 +15,7 @@ import re
 import shutil
 import subprocess
 import sys
+from collections import defaultdict
 from pathlib import Path
 
 import click
@@ -71,6 +73,12 @@ _logger = logging.getLogger().getChild(__name__)
     ),
 )
 @click.option(
+    "--floorplan-path",
+    type=Path,
+    default=None,
+    help="Path to the floorplan file. If specified, the floorplan will be applied.",
+)
+@click.option(
     "--vitis-mode / --no-vitis-mode",
     type=bool,
     default=True,
@@ -87,6 +95,7 @@ def analyze(
     top: str,
     cflags: tuple[str, ...],
     flatten_hierarchy: bool,
+    floorplan_path: Path | None,
     vitis_mode: bool,
 ) -> None:
     """Analyze TAPA program and store the program description."""
@@ -116,6 +125,10 @@ def analyze(
     tapa_graph = TapaGraph(None, graph_dict)
     if flatten_hierarchy:
         tapa_graph = tapa_graph.get_flatten_graph()
+    if floorplan_path:
+        assert flatten_hierarchy, "Floorplan can only be applied to a flattened graph"
+        slot_to_insts = get_slot_to_inst(floorplan_path, tapa_graph)
+        tapa_graph = tapa_graph.get_floorplan_graph(slot_to_insts)
     graph_dict = tapa_graph.to_dict()
     store_tapa_program(Program(graph_dict, vitis_mode, work_dir))
 
@@ -123,6 +136,23 @@ def analyze(
     store_persistent_context("settings", {"vitis-mode": vitis_mode})
 
     is_pipelined("analyze", True)
+
+
+def get_slot_to_inst(floorplan_path: Path, graph: TapaGraph) -> dict[str, list[str]]:
+    """Get slot to instance mapping from floorplan file."""
+    with open(floorplan_path, encoding="utf-8") as f:
+        vertex_to_region = json.load(f)
+    slot_to_insts = defaultdict(list)
+    task_inst_names = [
+        inst.name for inst in graph.get_top_task_inst().get_subtasks_insts()
+    ]
+    for vertex, region in vertex_to_region.items():
+        if vertex not in task_inst_names:
+            continue
+        slot_name = "_".join(region.split(":"))
+        slot_to_insts[slot_name].append(vertex)
+
+    return slot_to_insts
 
 
 def find_clang_binary(name: str) -> str:
