01744ed4c2b068821607955f9ea8eb66bc81a0cc
Crystal-RainSlide <16851802+Crystal-RainSlide@users.noreply.github.com>
2025-04-08 03:39:51 +0800

    feat(gui): use svgo for inline svg

--- tapa-visualizer/vite.config.ts
+++ tapa-visualizer/vite.config.ts
@@ -2,11 +2,58 @@
 // All rights reserved. The contributor(s) of this file has/have agreed to the
 // RapidStream Contributor License Agreement.
 
-import type { UserConfig } from "vite";
+import { type Config as SVGOConfig, optimize } from "svgo";
+import type { PluginOption, UserConfig } from "vite";
 import prismjsPlugin from "vite-plugin-prismjs-plus";
 
 import { join } from "node:path";
 import { readFileSync } from "node:fs";
+
+const lucideInlineSVGPlugin = (): PluginOption => {
+  const name = "vite-plugin-lucide-inline-svg";
+
+  const svgoConfig: SVGOConfig = {
+    // TODO: investgate whether the "path" option is needed here
+    plugins: [{
+      name: "preset-default",
+      params: {
+        overrides: {
+          // See: https://svgo.dev/docs/plugins/removeViewBox/
+          removeViewBox: false,
+          // Don't sort class="lucide lucide-xxx" to the last
+          sortAttrs: false,
+        }
+      },
+    }],
+  };
+
+  let root = "";
+
+  // TODO: apply this replacement in some virtual DOM instead
+  // From: <svg use="x"></svg>
+  // To:   <svg class="lucide lucide-x" ...>...</svg>
+  const transformHtml = (html: string) => html.replace(
+    /<svg\s+use="(.+?)"><\/svg>/g,
+    (match: string, icon: string) => {
+      const path = `node_modules/lucide-static/icons/${icon}.svg`;
+      try {
+        const svg = readFileSync(join(root, path), "utf-8");
+        return optimize(svg, svgoConfig).data;
+      } catch (e) {
+        console.warn(`${name}: Lucide SVG not exist or failed to read`, e);
+        return match;
+      }
+    },
+  );
+
+  return {
+    name,
+    configResolved: config => { root = config.root ?? ""; },
+    transformIndexHtml: transformHtml,
+    transform:
+      (code, id) => id.endsWith(".html") ? transformHtml(code) : undefined,
+  };
+};
 
 export default {
   build: {
@@ -14,41 +61,12 @@ export default {
     sourcemap: true,
   },
   plugins: [
-
     prismjsPlugin({
       manual: true,
       css: true,
       languages: ["cpp"],
       plugins: ["line-numbers"],
     }),
-
-    (() => {
-      const name = "vite-plugin-lucide-inline-svg";
-      const lucideStatic = (icon: string) =>
-        `node_modules/lucide-static/icons/${icon}.svg`;
-
-      const minifyLucideSVG = (lucideSVG: string) =>
-        lucideSVG
-          .replace(/<!-- @license lucide-static v[0-9.]+ - ISC -->\n/, "")
-          .replace(/\n\s+/g, " ")
-          .replaceAll("\n", "")
-          .replaceAll("> <", "><")
-          .replaceAll(" />", "/>");
-
-      let root = "";
-
-      return {
-        name,
-        configResolved: config => { root = config.root ?? ""; },
-        // <svg use="x"></svg> -> <svg class="lucide lucide-x" ...>...</svg>
-        transformIndexHtml: html => html.replace(
-          /<svg\s+use="(.+?)"><\/svg>/g,
-          (_match, icon: string) => minifyLucideSVG(
-            readFileSync(join(root, lucideStatic(icon)), "utf-8")
-          ),
-        ),
-      };
-    })(),
-
+    lucideInlineSVGPlugin(),
   ],
 } satisfies UserConfig;
