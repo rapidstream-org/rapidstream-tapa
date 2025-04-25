55a56655e2676b7a452949bcec27255961ee39fe
Jason Lau <lau@rapidstream-da.com>
2025-04-25 12:32:32 -0700

    build(nuitka): pack shared atomic library

--- bazel/nuitka_wrapper.py
+++ bazel/nuitka_wrapper.py
@@ -9,6 +9,8 @@ RapidStream Contributor License Agreement.
 import os
 import runpy
 
+import nuitka.PythonVersions
+
 if "BUILD_WORKING_DIRECTORY" in os.environ:
     os.chdir(os.environ["BUILD_WORKING_DIRECTORY"])
 
@@ -29,6 +31,12 @@ for key in (
         os.environ[key] = os.pathsep.join(
             os.path.abspath(p) for p in os.environ[key].split(os.pathsep)
         )
+
+# Monkey-patch Nuitka to use -latomic instead of its static library.
+ORIGINAL_MODULE_LINKER_LIBS = nuitka.PythonVersions.getModuleLinkerLibs
+nuitka.PythonVersions.getModuleLinkerLibs = lambda: [
+    lib.replace(":libatomic.a", "atomic") for lib in ORIGINAL_MODULE_LINKER_LIBS()
+]
 
 os.environ["NUITKA_CACHE_DIR"] = "/tmp/.nuitka_cache"
 
