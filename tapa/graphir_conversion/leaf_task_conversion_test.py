ba279af5178d496faffe7788bde42934fa92c9c9
Ed-5100 <yx388@rapidstrea-da.com>
2025-05-09 13:13:18 -0400

    test: added verilog module conversion test

---
+++ tapa/graphir_conversion/leaf_task_conversion_test.py
@@ -0,0 +1,48 @@
+"""Test for leaf task rapidstream graphir conversion."""
+
+__copyright__ = """
+Copyright (c) 2025 RapidStream Design Automation, Inc. and contributors.
+All rights reserved. The contributor(s) of this file has/have agreed to the
+RapidStream Contributor License Agreement.
+"""
+
+import json
+import os
+from pathlib import Path
+
+import pytest
+
+from tapa.graphir_conversion.gen_rs_graphir import get_verilog_module_from_leaf_task
+from tapa.task import Task
+from tapa.verilog.xilinx.module import Module
+
+CURR_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+@pytest.mark.parametrize(
+    "name",
+    ["Add", "ProcElem"],
+)
+def test_leaf_task_conversion(name: str) -> None:
+    """Test leaf task conversion."""
+    task = Task(
+        name=name,
+        code="",
+        level="lower",
+        tasks=None,
+        fifos=None,
+        ports=None,
+        target_type="hls",
+        is_slot=False,
+    )
+    task.module = Module(
+        name=name,
+        files=[str(Path(CURR_DIR) / f"test_files/{name}.v")],
+        is_trimming_enabled=task.is_lower,
+    )
+    generated = get_verilog_module_from_leaf_task(task).model_dump_json()
+    with open(
+        os.path.join(CURR_DIR, f"test_files/{name}_golden.json"), encoding="utf-8"
+    ) as f:
+        golden = json.load(f)
+    assert json.loads(generated) == golden, generated
