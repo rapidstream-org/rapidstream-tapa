fcdc57a7195830d4f718a4355c297a45b460d677
Jason Lau <lau@rapidstream-da.com>
2025-05-29 16:27:40 -0700

    chore: fix pre-commit

--- tapacc/tapacc.cpp
+++ tapacc/tapacc.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2024 RapidStream Design Automation, Inc. ane contributors.
+// Copyright (c) 2025 RapidStream Design Automation, Inc. and contributors.
 // All rights reserved. The contributor(s) of this file has/have agreed to the
 // RapidStream Contributor License Agreement.
 
