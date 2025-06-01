5b1efadeeb1bc0ceaf3d6c27c57c6497e27010c3
Blaok <i@blaok.me>
2025-05-31 22:55:28 -0700

    test(libtapa): recover unittest of `tapa::hls::stream`
    Partially reverts commit cbc143b6ac41784f87cc30d7ca6e6fcb6b2a8849.

---
+++ tapa-lib/tapa/compat_test.cpp
@@ -0,0 +1,83 @@
+// Copyright (c) 2024 RapidStream Design Automation, Inc. and contributors.
+// All rights reserved. The contributor(s) of this file has/have agreed to the
+// RapidStream Contributor License Agreement.
+
+#include "tapa/compat.h"
+
+#include <gtest/gtest.h>
+
+namespace {
+
+constexpr int kN = 5000;
+constexpr int kDepth = 5;
+
+void DataSink(tapa::hls::stream<int>& data_in_q, int n) {
+  for (int i = 0; i < n; ++i) {
+    data_in_q.read();
+  }
+}
+
+void DataSource(tapa::hls::stream<int>& data_out_q, int n) {
+  for (int i = 0; i < n; ++i) {
+    data_out_q.write(i);
+  }
+}
+
+TEST(SimulateTest, SimulateHlsStreamHasInfiniteDepth) {
+  tapa::hls::stream<int> data_q;
+  DataSource(data_q, 10'000'000);
+}
+
+TEST(SimulateTest, SimulateHlsStreamCanBeNamed) {
+  tapa::hls::stream<int> data_q("data");
+  DataSource(data_q, kN);
+  DataSink(data_q, kN);
+}
+
+TEST(SimulateTest, SimulateHlsStreamCanBeInvoked) {
+  tapa::hls::stream<int> data_q("data");
+  tapa::task().invoke(DataSource, data_q, kN).invoke(DataSink, data_q, kN);
+}
+
+void DataSourceNonBlocking(tapa::ostream<float>& data_out_q,
+                           tapa::hls::stream<int>& count_out_q, int n) {
+  int write_count = 0;
+  for (int i = 0; i < n; ++i) {
+    if (data_out_q.try_write(i)) {
+      ++write_count;
+    }
+  }
+  count_out_q.write(write_count);
+}
+
+void DataSinkNonBlocking(tapa::istream<float>& data_in_q,
+                         tapa::hls::stream<int>& count_in_q, int n) {
+  int read_count = 0;
+  for (int i = 0; i < n; ++i) {
+    float value_unused;
+    if (data_in_q.try_read(value_unused)) {
+      ++read_count;
+    }
+  }
+
+  // Since `tapa::hls_simulate:task()` schedules tasks sequentially, we should
+  // only be able to read `kDepth` elements.
+  EXPECT_EQ(read_count, kDepth);
+
+  // `DataSourceNonBlocking` should have finished here so we expect to read the
+  // count successfully.
+  int write_count = -1;
+  EXPECT_TRUE(count_in_q.try_read(write_count));
+
+  // The `write_count` should be the same as `read_count`.
+  EXPECT_EQ(write_count, read_count);
+}
+
+TEST(SimulateTest, SimulateHlsTasksAreScheduledSequentially) {
+  tapa::stream<float, kDepth> data_q;
+  tapa::hls::stream<int> count_q;
+  DataSourceNonBlocking(data_q, count_q, kN);
+  DataSinkNonBlocking(data_q, count_q, kN);
+}
+
+}  // namespace
