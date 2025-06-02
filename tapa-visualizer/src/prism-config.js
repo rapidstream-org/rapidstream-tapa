1e49cff001762c9b9663c955300d23cd9c1aa8c7
Crystal-RainSlide <16851802+Crystal-RainSlide@users.noreply.github.com>
2025-06-02 15:24:41 +0800

    fix(gui): add prism line number css

--- tapa-visualizer/src/prism-config.js
+++ tapa-visualizer/src/prism-config.js
@@ -9,6 +9,8 @@ import Prism from "prismjs";
 import "prismjs/themes/prism.css";
 import "prismjs/components/prism-c.js";
 import "prismjs/components/prism-cpp.js";
+
 import "prismjs/plugins/line-numbers/prism-line-numbers.js";
+import "prismjs/plugins/line-numbers/prism-line-numbers.css";
 
 export default Prism;
