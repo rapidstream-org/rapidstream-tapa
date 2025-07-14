aab6fe79962da5fbb468cf373e8ebf3e9f7c91ca
Ed-5100 <yx388@rapidstrea-da.com>
2025-07-14 18:00:51 -0400

    feat(steps): added add-pipeline flag to floorplan-compile cmd

--- tapa/steps/meta.py
+++ tapa/steps/meta.py
@@ -9,24 +9,47 @@ RapidStream Contributor License Agreement.
 
 import click
 
+from tapa.steps.add_pipeline import add_pipeline
 from tapa.steps.analyze import analyze
 from tapa.steps.common import forward_applicable
-from tapa.steps.floorplan import run_autobridge
+from tapa.steps.floorplan import floorplan, run_autobridge
 from tapa.steps.pack import pack
 from tapa.steps.synth import synth
 
 
 @click.command("compile")
+@click.option(
+    "--run-add-pipeline",
+    is_flag=True,
+    help=(
+        "Run the add pipeline step in the compilation flow. When specified, "
+        "flatten_hierarchy and gen_graphir options are specified automatically."
+    ),
+)
 @click.pass_context
-def compile_entry(ctx: click.Context, **kwargs: dict) -> None:
+def compile_entry(ctx: click.Context, run_add_pipeline: bool, **kwargs: dict) -> None:
     """Compile a TAPA program to a hardware design."""
-    forward_applicable(ctx, analyze, kwargs)
-    forward_applicable(ctx, synth, kwargs)
-    forward_applicable(ctx, pack, kwargs)
+    if not run_add_pipeline:
+        forward_applicable(ctx, analyze, kwargs)
+        forward_applicable(ctx, synth, kwargs)
+        forward_applicable(ctx, pack, kwargs)
+    else:
+        assert "floorplan_path" in kwargs, (
+            "When --run-add-pipeline is specified, floorplan_path must be provided."
+        )
+        kwargs["flatten_hierarchy"] = True
+        kwargs["gen_graphir"] = True
+        forward_applicable(ctx, analyze, kwargs)
+        forward_applicable(ctx, floorplan, kwargs)
+        forward_applicable(ctx, synth, kwargs)
+        forward_applicable(ctx, add_pipeline, kwargs)
+        forward_applicable(ctx, pack, kwargs)
 
 
 compile_entry.params.extend(analyze.params)
+compile_entry.params.extend(floorplan.params)
 compile_entry.params.extend(synth.params)
+compile_entry.params.extend(add_pipeline.params)
 compile_entry.params.extend(pack.params)
 
 
@@ -45,23 +68,3 @@ def generate_floorplan_entry(ctx: click.Context, **kwargs: dict) -> None:
 generate_floorplan_entry.params.extend(analyze.params)
 generate_floorplan_entry.params.extend(synth.params)
 generate_floorplan_entry.params.extend(run_autobridge.params)
-
-
-@click.command("post-floorplan-compile")
-@click.pass_context
-def post_floorplan_compile_entry(ctx: click.Context, **kwargs: dict) -> None:
-    """Compile a TAPA program to a hardware design with post-floorplanning."""
-    kwargs["flatten_hierarchy"] = True
-    kwargs["gen_graphir"] = True
-    forward_applicable(ctx, analyze, kwargs)
-    forward_applicable(ctx, floorplan, kwargs)
-    forward_applicable(ctx, synth, kwargs)
-    forward_applicable(ctx, add_pipeline, kwargs)
-    forward_applicable(ctx, pack, kwargs)
-
-
-post_floorplan_compile_entry.params.extend(analyze.params)
-post_floorplan_compile_entry.params.extend(floorplan.params)
-post_floorplan_compile_entry.params.extend(synth.params)
-post_floorplan_compile_entry.params.extend(add_pipeline.params)
-post_floorplan_compile_entry.params.extend(pack.params)
